897c57404fdc972bcc9c66a8d43c0e036e024e15
Admin <Admin@TEB12701>
2023-09-22 13:22:50 +0200

    Add project files.

---
+++ 3.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Övning_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >= 50)
+            {
+                Console.WriteLine("Är du en man?");
+                string kön = (Console.ReadLine().ToLower());
+                if (kön == "ja")
+                {
+                    Console.WriteLine("Grattis! du får delta :)");
+                }
+                if (kön == "nej")
+                {
+                    Console.WriteLine("Synd för dig");
+                    Console.ReadKey();
+                    Console.WriteLine("Bara skoja, vill du verkligen vara med ändå?");
+
+                    string svar = Console.ReadLine().ToLower();
+
+                    if (svar == "ja")
+                    {
+                        Console.WriteLine("Okej då, du får delta ändå");
+                    }
+                    if (svar == "nej")
+                    {
+                        Console.WriteLine("Nähe, synd för dig");
+                    }
+                }
+            }
+            if (ålder < 50)
+            {
+                Console.WriteLine("Synd för dig");
+            }
+            Console.ReadKey();
+        }
+    }
+}
